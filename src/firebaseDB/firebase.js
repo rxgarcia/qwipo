09a89f28d222bbf6a637fee0c8c75089a0c867e0
Rigo <rigo81401@gmail.com>
2022-01-10 15:46:20 -0600

    update

--- src/firebaseDB/firebase.js
+++ src/firebaseDB/firebase.js
@@ -6,13 +6,13 @@ import {useFirestoreEmulator as applyFirestoreEmulator} from 'firebase/firestore
 import { getAuth, useAuthEmulator as applyAuthEmulator} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBqs0MTeO0Lh1PC75JPuQh_Louw0RrsanA",
-  authDomain: "react-http-tutorial-a99e8.firebaseapp.com",
-  databaseURL: "https://react-http-tutorial-a99e8-default-rtdb.firebaseio.com",
-  projectId: "react-http-tutorial-a99e8",
-  storageBucket: "react-http-tutorial-a99e8.appspot.com",
-  messagingSenderId: "347451132845",
-  appId: "1:347451132845:web:e1113af13dd4c8bb5cef24",
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 };
 
 // auth imports
